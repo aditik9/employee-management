d5d445af3b6094bba6e8ab6a045da01f2632b772
aditik9 <82046941+aditik9@users.noreply.github.com>
2022-03-27 19:16:39 +0530

    basic website structure created

---
+++ employee-app/src/component/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='Page-heading'>
+        <h1>Employee Management</h1>
+      </div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Header
---
+++ employee-app/src/component/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navigation-bar'>
+        <div className='Nav-buttons'>
+            <button>Home</button>
+            <button>Employee</button>
+            <button>Others</button>
+            <button>Logout</button>
+        </div>
+    </div>
+  )
+}
+export default Navbar
